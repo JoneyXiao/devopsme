eeaa279fb5f8cf674a7cb26b2468e170aab3dd83
Joney Xiao <87732444@qq.com>
2025-05-28 23:18:41 +0800

    fix: set base to repo name

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/devopsme/",
 
   lang: "en-US",
   title: "DevOpsMe",
